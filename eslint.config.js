9d556eeab3744975822e575eda561882a7f10912
Andi Ruci <andiruci.131@gmail.com>
2026-05-19 02:13:25 +0200

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'node_modules',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -14,8 +17,16 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
 ])
